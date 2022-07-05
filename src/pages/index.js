03b24d97c2739486b1e835f63a8bb3dbc92908fd
corlard3y <corlardey@gmail.com>
2022-07-05 15:12:47 +0100

    optimize fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import Home from "./home";
 
 export default function index() {
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href =
+      "https://fonts.googleapis.com/css2?family=DM+Sans&display=swap";
+    // link.integrity =
+    //   "sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z";
+    link.crossOrigin = "anonymous";
+    document.head.appendChild(link)
+  }, [])
   return (
     <>
       <Head>
